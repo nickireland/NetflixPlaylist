1870b96964269480f8c3415cf337bd83f56e256c
Nick Ireland <nick.a.ireland@gmail.com>
2019-09-13 22:56:19 -0700

    version sends message from bckgr to content & back

--- background.js
+++ background.js
@@ -3,10 +3,19 @@
     "title": "AddToPlaylist",
     "contexts": ['video'] 
 });*/
-chrome.webNavigation.onCompleted.addListener(function(details){
+
+/*chrome.webNavigation.onCompleted.addListener(function(details){
     chrome.tabs.executeScript(details.tabId, {
         file: "content_script.js"
     });
 }, {
     url: [{"hostContains": "netflix.com"}]
+});*/
+
+chrome.webNavigation.onCompleted.addListener(function(details){
+    chrome.tabs.executeScript(details.tabId,{
+        file: "content_script.js"
+    }, () => {
+        chrome.tabs.sendMessage(details.tabId, { magic_word: " INDIGO ZOMBIE"}, txt => alert("called me back: " + txt))
+    });
 });
--- content_script.js
+++ content_script.js
@@ -1,2 +1,7 @@
-alert(document.querySelector("h1").innerText);
+chrome.runtime.onMessage.addListener( (message, sender, cb) => {
+    alert(message.magic_word);
+    cb(document.querySelector("h1").innerText);
+});
+
+//alert(document.querySelector("h1").innerText);
 //alert("test 2");
